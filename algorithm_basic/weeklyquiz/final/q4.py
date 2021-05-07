86fd5254a5a2b9c5016f54116144271f294138ed
HyunSeokJeon <sjunhs@gmail.com>
2021-05-07 17:16:47 +0900

    Update q4.py

--- algorithm_basic/weeklyquiz/final/q4.py
+++ algorithm_basic/weeklyquiz/final/q4.py
@@ -5,38 +5,46 @@ def solution(N, duration, cost):
  
     def dynamic_programming():
         max_val = 0
+        sumdp = 0
+        sumcomp = 0
         for i in range(N-1, -1, -1):
             if i+duration[i] > N:
                 continue
             comp = dp.copy()
-            # comp[i] = 1
-            # for j in range(i, i+duration[i]):
-            #     comp[j] = 0
-            # sumdp = 0
-            # sumcomp = 0
-            # for j in range(i, N+1):
-            #     sumdp += dp[j] * cost[j]
-            #     sumcomp += comp[j] * cost[j]
-            # if sumdp < sumcomp:
-            #     dp = comp
-
-            print(dp)
-            print('duration',duration[i])
-            print('cost', cost[i])
-            print(i)
- 
-        return max_val
- 
+            comp[i] = 1
+            for j in range(i+1, i+duration[i]):
+                comp[j] = 0
+            sumdp = 0
+            sumcomp = 0
+            for j in range(len(comp)-1):
+                sumdp += dp[j] * cost[j]
+                sumcomp += comp[j] * cost[j]
+            if sumdp <= sumcomp:
+                dp[i] = 1
+                for j in range(i+1, i+duration[i]):
+                    dp[j] = 0
+            print(comp)
+    
     result = dynamic_programming()
+    sumdp = 0; 
+    for i in range(len(dp)-1):
+        sumdp += dp[i] * cost[i]
+    result = sumdp
     return result
+
+N = int(input())
+duration = []
+cost = []
+for _ in range(N):
+    d, c = list(map(int,input().split()))
+    duration.append(d)
+    cost.append(c)
  
- 
-N = 7
-duration = [3, 5, 1, 1, 2, 4, 2]
-cost = [10, 20, 10, 20, 15, 40, 200]
 print(solution(N, duration, cost))
- 
-N = 10
-duration = [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
-cost = [50, 40, 30, 20, 10, 10, 20, 30, 40, 50]
-print(solution(N, duration, cost))
+
+# 5
+# 3 50
+# 1 1
+# 3 3
+# 2 2 
+# 1 1
